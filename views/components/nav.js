60cc97b2dbac527b4b7ec5ab126145af89dbfe10
Joachim Perez <joachim3008@gmail.com>
2026-06-15 22:10:42 -0400

    notification

--- views/components/nav.js
+++ views/components/nav.js
@@ -21,43 +21,6 @@ const createNavHome = () => {
                         </div>`;
 };
 
-
-// const createNavHome = () => {
-
-//   const createNvLogin = () => {}
-//     navbar.innerHTML = `
-//     <div class="max-w-7xl bg-green-400  h-16 mx-auto flex items-center px-4 justify-between">
-//         <p class="font-bold text-xl text-white">TodoApp</p>
-
-//         <!--version movil-->
-        
-//         <svg xmlns="http://www.w3.org/2000/svg" 
-//         fill="none" viewBox="0 0 24 24" 
-//         stroke-width="1.5" 
-//         stroke="currentColor" 
-//         id="navBtn"
-//         class="w-10 h-10 md:hidden cursor-pointer p-2 rounded-lg">
-//           <path 
-//           stroke-linecap="round" 
-//           stroke-linejoin="round" 
-//           d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25" 
-//           />
-//         </svg>
-
-
-//         <!-- version de escritorio -->
-//          <div class="hidden md:flex flex-row gap-4">
-//           <a href="/signup/" class="transition ease-in-out text-white font-bold bg-green-400 hover:bg-green-500 py-2 px-4 rounded-lg">Sign Up</a>         </div>
-
-//         <!-- menu movil -->
-
-//           <div id="menuMobile" class="bg-slate-900 fixed top-16 right-0 left-0 bottom-0 justify-center items-center flex-col gap-4 hidden md:hidden">
-//           <a href="/signup/" class="transition ease-in-out text-white font-bold bg-green-400 hover:bg-green-500 py-2 px-4 rounded-lg">Sign Up</a>
-//           </div>
-//       </div>
-//     `;
-// };
-
 const createNavSignup = () => {
        navbar.innerHTML = `<div class="max-w-7xl bg-green-400  h-16 mx-auto flex items-center px-4 justify-between">
          <a href="/" class="font-bold text-xl text-white">TodoApp</a>
@@ -71,7 +34,7 @@ const createNavSignup = () => {
                                 
                             <!-- version de escritorio -->
                             <div class="hidden md:flex flex-row gap-4">
-                                <a href="/login/" class="transition ease-in-out text-white font-bold hover:bg-indigo-800 py-2 px-4 rounded-lg">Log In</a>
+                                <a href="/login" class="transition ease-in-out text-white font-bold bg-indigo-500 hover:bg-indigo-800 py-2 px-4 rounded-lg">Log In</a>
                             </div>
                         </div>`;
 };
--- views/signup/index.js
+++ views/signup/index.js
@@ -1,10 +1,75 @@
+import { createNotification } from "../components/notification.js";
+
+const REGEX_PASSWORD = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+const REGEX_USERNAME = /^[a-zA-Z0-9_]{3,16}$/;
+const REGEX_PHONE = /^(\+?\d{1,3})?(\d{7,12})$/;
+const REGEX_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+
 const form = document.getElementById("form");
 const nameInput = document.getElementById("name-input");
 const emailInput = document.getElementById("email-input");
 const passwordInput = document.getElementById("password-input");
+const matchInput = document.getElementById("match-input");
+const formBtn = document.getElementById("form-btn");
+const notification = document.getElementById("notification");
 
 
-//recuerda poner el boton en disabled para evitar que se envie el formulario sin llenar los campos
+//validations
+
+let nameValidation = false;
+let emailValidation = false;
+let passwordValidation = false;
+let matchValidation = false;
+
+const validation = (input, regexValidation) => {
+
+  
+  if (input.value === "") {
+    input.classList.remove("outline-red-500", "outline-2", "outline");
+    input.classList.remove("outline-green-500", "outline-2", "outline");
+    input.classList.add("focus:outline-indigo-700");
+  }
+  else if (!regexValidation) {
+    input.classList.remove("focus:outline-indigo-700");
+    input.classList.add("outline-red-500", "outline-2", "outline");
+  } else {
+    input.classList.remove("outline-red-500", "outline-2", "outline");
+    input.classList.add("outline-green-500", "outline-2", "outline");
+  };
+
+  formBtn.disabled = (nameValidation && emailValidation && passwordValidation && matchValidation) ? false : true;
+  console.log(nameValidation && emailValidation && passwordValidation && matchValidation);
+};
+
+
+//events
+
+nameInput.addEventListener("input", e => {
+  nameValidation = REGEX_USERNAME.test(nameInput.value);
+  validation(nameInput, nameValidation);
+});
+
+emailInput.addEventListener("input", e => {
+  emailValidation = REGEX_EMAIL.test(emailInput.value);
+  validation(emailInput, emailValidation);
+});
+
+passwordInput.addEventListener("input", e => {
+  passwordValidation = REGEX_PASSWORD.test(passwordInput.value);
+  matchValidation = e.target.value === matchInput.value;
+  validation(passwordInput, passwordValidation);
+  validation(matchInput, matchValidation);
+});
+
+matchInput.addEventListener("input", e => {
+  matchValidation = e.target.value === passwordInput.value;
+  validation(matchInput, matchValidation);
+});
+
+
+
+// recuerda poner el boton en disabled para evitar que se envie el formulario sin llenar los campos
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -17,6 +82,9 @@ form.addEventListener("submit", async (e) => {
     const response = await axios.post("/api/users", newUser);
     console.log(response);
   } catch (error) {
-    console.error(error);
+    createNotification(true, error.response.data.error);
+    setTimeout(() => {
+      notification.classList.add("hidden");
+    }, 5000);
   }
 });
