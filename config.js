3198b18288d64a28db994b7ca742d3a706e10470
Joachim Pérez <joachim3008@gmail.com>
2026-07-14 14:14:13 -0400

    fix: agregar script start en package.json

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
- const PAGE_URL = process.env.NODE_ENV === "production" ? "placeholder" : "http://localhost:3000";
+const PAGE_URL = process.env.NODE_ENV === "production" ? "placeholder" : "http://localhost:3000";
+const MONGO_URI = process.env.NODE_ENV === "production" ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_TEST;
+
 
 module.exports = {
   PAGE_URL,
+  MONGO_URI
 };
