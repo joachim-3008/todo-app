c969691a46b15f1c33036567d2e059d61ee8f3d9
Joachim Pérez <joachim3008@gmail.com>
2026-07-14 14:20:36 -0400

    fixed add link prod

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PAGE_URL = process.env.NODE_ENV === "production" ? "placeholder" : "http://localhost:3000";
+const PAGE_URL = process.env.NODE_ENV === "production" ? "https://todo-app-7i8z.onrender.com" : "http://localhost:3000";
 const MONGO_URI = process.env.NODE_ENV === "production" ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_TEST;
 
 
